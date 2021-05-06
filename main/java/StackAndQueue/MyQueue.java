034dd035d01c181a620a2b803d863b1f0ada37cc
Abhishek <ABHISHEKKUMAR998>
2021-05-06 20:45:27 +0530

    ADD dequeue

--- main/java/StackAndQueue/MyQueue.java
+++ main/java/StackAndQueue/MyQueue.java
@@ -2,16 +2,29 @@ package StackAndQueue;
 
 public class MyQueue<K extends Comparable<K>> {
 	private final MyLinkedList<K> myLinkedList;
+	
+	
 	public MyQueue() {
 		myLinkedList = new MyLinkedList<>();
 	}
+	
+	
 	public void enqueu(INode<K> element) {
 		myLinkedList.append(element);
 	}
+	
+	
 	public INode<K> peak() {
 		return myLinkedList.head;
 	}
+	
+	
 	public void printQueue() {
 		myLinkedList.printMyNode();
+	}
+	
+	
+	public INode<K> dequeue() {
+		return myLinkedList.pop();
 	}
 } 
